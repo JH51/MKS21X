15bddb2b83d8044202c7b81e0da61a44c4f4fb50
Jonathan Huang <jonhuang5@gmail.com>
2016-12-07 05:40:21 -0500

    161207 14:37

---
+++ 03Barcode/Barcode.java
@@ -0,0 +1,22 @@
+public class Barcode implements Comparable<Barcode> {
+
+    private String _zip;
+    private int _checkDigit;
+
+    public Barcode(String zip) {
+	try {
+	    this._zip = zip;
+	    this._checkDigit = 0;
+	    for (int index = 0; index < this._zip.length(); index += 1) {
+		this._checkDigit += Integer.parseInt(this._zip.charAt(index));
+	    }
+	    this._checkDigit %= 10;
+	}
+	catch(StringIndexOutOfBoundsException e) {
+	}
+
+    public Barcode clone() {
+	
+    }
+
+    
