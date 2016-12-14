8efea6284d656c3b4ee9d91a2fa1064dd2ec87c3
JH51 <jonhuang5@gmail.com>
2016-12-13 19:45:33 -0500

    161213

---
+++ 04Sorts/Sorts.java
@@ -0,0 +1,39 @@
+public class Sorts{
+
+    public static String name(){
+      return "09.Huang.Jonathan"; 
+    }
+
+    private static boolean sorted(int[] data) {
+	boolean booleanSorted = true;
+	for (int i = 1; i < data.length && booleanSorted; i += 1) {
+	    booleanSorted = data[i] > data[i - 1];
+	}
+	return booleanSorted;
+    }
+
+    private static void swap(int[] data, int a, int b) {
+	int A = data[a];
+	int B = data[b];
+	data[a] = B;
+	data[b] = A;
+    }
+    
+    public static void selectionSort(int[] data) {
+	int min = data[0];
+	int pos1 = 0;
+	int pos2 = 0;
+	while (! sorted(data) && pos1 < data.length) {
+	    min = data[data.length - 1];
+	    for (int i = pos1 + 1; i < data.length; i += 1) {
+		if (data[i] <= min) {
+		    min = data[i];
+		    pos2 = i;
+		}
+	    }
+	    System.out.println(pos1 + " " + pos2 + "  ");
+	    swap(data, pos1, pos2);
+	    pos1 += 1;}		
+    }
+
+}
